5345603f7a4c9703fbec132a3c9f58461efbc5d4
mldxo <miloszmaculewicz@gmail.com>
2024-09-12 17:30:24 +0200

    Adjust background style

--- frontend/src/pages/TemplatePage.jsx
+++ frontend/src/pages/TemplatePage.jsx
@@ -1,8 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import PropTypes from 'prop-types';
-import ThemeToggleButton from "../components/ThemeToggleButton";
-import LanguageToggleButton from "../components/LanguageToggleButton";
+import ThemeToggleButton from "../functions/themeToggleButton";
+import LanguageToggleButton from "../functions/languageToggleButton";
 import CookieConsent from "react-cookie-consent";
 import { useTranslation } from "react-i18next";
 
@@ -17,7 +17,7 @@ function TemplatePage({ children }) {
 		<div className="flex flex-col min-h-screen font-inter transition-all duration-200">
 			<ThemeToggleButton />
 			<LanguageToggleButton />
-			<div className="flex flex-col flex-grow justify-center justify-items-center bg-blue-200/[.8] dark:bg-gray-900/[.95] dark:text-white -mt-2">
+			<div className="flex flex-col flex-grow justify-center justify-items-center bg-gradient-to-r from-[rgba(108,150,241,0.55)] to-[rgba(78,129,238,0.56)] dark:from-[rgba(80,86,143,0.86)] dark:to-[rgba(44,91,151,0.79)] dark:text-white -mt-2">
 				{children}
 			</div>
 			<div id="cookie-consent">
