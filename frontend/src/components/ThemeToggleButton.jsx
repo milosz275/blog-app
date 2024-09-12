657327b4750ce7b9e87860c2d1eb29e2544e7ea1
mldxo <miloszmaculewicz@gmail.com>
2024-09-12 16:34:29 +0200

    Adjust background color

--- frontend/src/components/ThemeToggleButton.jsx
+++ frontend/src/components/ThemeToggleButton.jsx
@@ -8,7 +8,7 @@ function ThemeToggleButton() {
     <div className="fixed left-4 top-5 z-50">
       <button
         onClick={toggleTheme}
-        className="w-14 h-8 p-1 bg-white dark:bg-gray-700 hover:drop-shadow-lg dark:hover:drop-shadow-lg text-lg rounded-full flex items-center justify-between"
+        className="w-14 h-8 p-1 bg-slate-100 dark:bg-gray-700 hover:drop-shadow-lg dark:hover:drop-shadow-lg text-lg rounded-full flex items-center justify-between"
       >
         <div
           className={`w-6 h-6 rounded-full shadow-md transform duration-300 ease-in-out ${
