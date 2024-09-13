4ab37023a553357faf7d972fe1acc100ac202a62
mldxo <miloszmaculewicz@gmail.com>
2024-09-13 17:36:05 +0200

    Update

--- frontend/src/urls.js
+++ frontend/src/urls.js
@@ -12,19 +12,20 @@ export const API_BASE_URL = API_BASE;
 // frontend
 
 //// shared
-export const ROOT_URL = "/";
-export const DEBUG_URL = "/debug";
+let BASE_URL = "/blog-app";
+export const ROOT_URL = BASE_URL + "/";
+export const DEBUG_URL = BASE_URL + "/debug";
 
 //// en
-export const POSTS_URL = "/posts";
-export const LOGIN_URL = "/login";
-export const REGISTER_URL = "/register";
-export const PROFILE_URL = "/profile";
-export const LOGOUT_URL = "/logout";
+export const POSTS_URL = BASE_URL + "/posts";
+export const LOGIN_URL = BASE_URL + "/login";
+export const REGISTER_URL = BASE_URL + "/register";
+export const PROFILE_URL = BASE_URL + "/profile";
+export const LOGOUT_URL = BASE_URL + "/logout";
 
 //// pl
-export const POSTS_URL_PL = "/posty";
-export const LOGIN_URL_PL = "/logowanie";
-export const REGISTER_URL_PL = "/rejestracja";
-export const PROFILE_URL_PL = "/profil";
-export const LOGOUT_URL_PL = "/wyloguj";
+export const POSTS_URL_PL = BASE_URL + "/posty";
+export const LOGIN_URL_PL = BASE_URL + "/logowanie";
+export const REGISTER_URL_PL = BASE_URL + "/rejestracja";
+export const PROFILE_URL_PL = BASE_URL + "/profil";
+export const LOGOUT_URL_PL = BASE_URL + "/wyloguj";
