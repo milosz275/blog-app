2cf46bdcbf86019f7cbcfdfdbafc0e8e7979afee
mldxo <miloszmaculewicz@gmail.com>
2024-09-13 11:33:52 +0200

    Fill project urls

--- frontend/src/urls.js
+++ frontend/src/urls.js
@@ -10,5 +10,21 @@ if (debug) {
 export const API_BASE_URL = API_BASE;
 
 // frontend
+
+//// shared
 export const ROOT_URL = "/";
 export const DEBUG_URL = "/debug";
+
+//// en
+export const POSTS_URL = "/posts";
+export const LOGIN_URL = "/login";
+export const REGISTER_URL = "/register";
+export const PROFILE_URL = "/profile";
+export const LOGOUT_URL = "/logout";
+
+//// pl
+export const POSTS_URL_PL = "/posty";
+export const LOGIN_URL_PL = "/logowanie";
+export const REGISTER_URL_PL = "/rejestracja";
+export const PROFILE_URL_PL = "/profil";
+export const LOGOUT_URL_PL = "/wyloguj";
