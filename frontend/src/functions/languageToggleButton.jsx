665fb3775f8f5a9400d835dfd8934290f38baee5
mldxo <miloszmaculewicz@gmail.com>
2024-09-13 11:39:33 +0200

    Adjust effects

--- frontend/src/functions/languageToggleButton.jsx
+++ frontend/src/functions/languageToggleButton.jsx
@@ -13,7 +13,7 @@ function LanguageToggleButton() {
     <div className="fixed right-4 top-5 z-50">
       <button
         onClick={() => changeLanguage(i18n.language === "en" ? "pl" : "en")}
-        className="w-14 h-8 p-1 bg-slate-100 dark:bg-gray-700 text-lg rounded-full flex items-center justify-between"
+        className="w-14 h-8 p-1 bg-slate-100 dark:bg-gray-700 text-lg rounded-full flex items-center justify-between transition-all duration-300 ease-in-out transform"
       >
         <div
           className={`w-6 h-6 rounded-full shadow-md transform duration-300 ease-in-out transition-all ${i18n.language === "pl" ? "translate-x-6" : "translate-x-0"
