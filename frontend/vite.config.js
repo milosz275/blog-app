623e575d9dfeefabb92e5d0886123c86d6805913
mldxo <miloszmaculewicz@gmail.com>
2024-09-13 16:34:15 +0200

    Update config for GitHub "sub" Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/blog-app/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        '/blog-app/assets/index-BXGh_3uy.js',
+      ],
+    },
+  },
 })
