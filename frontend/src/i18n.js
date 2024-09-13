dacc385a92c6ba138a6a021d631e55d7ceeba8d9
mldxo <miloszmaculewicz@gmail.com>
2024-09-13 17:36:58 +0200

    Update

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -14,7 +14,7 @@ i18n
             escapeValue: false
         },
         backend: {
-            loadPath: "/locales/{{lng}}/translation.json"
+            loadPath: "/blog-app/locales/{{lng}}/translation.json"
         }
     });
 
