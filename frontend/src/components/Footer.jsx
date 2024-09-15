1d1b77861d18e1a4f5c4bc3ded75684fbaedfb9d
mldxo <miloszmaculewicz@gmail.com>
2024-09-15 15:05:02 +0200

    Add last deploy field

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,40 +4,52 @@ import { useTranslation } from "react-i18next";
 import { github } from "../icons";
 
 const Footer = () => {
-    const { t } = useTranslation();
+    const { i18n, t } = useTranslation();
     const [createdYear, setCreatedYear] = useState(null);
     const [avatarUrl, setAvatarUrl] = useState(null);
     const [isHovered, setIsHovered] = useState(false);
-    const [isAvatarLoaded, setIsAvatarLoaded] = useState(false); // New state to handle avatar loading
+    const [isAvatarLoaded, setIsAvatarLoaded] = useState(false);
+    const [lastPageDeploy, setLastPageDeploy] = useState(null);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch("https://api.github.com/users/mldxo");
-                const data = await response.json();
-                const year = new Date(data.created_at).getFullYear();
+                const userResponse = await fetch("https://api.github.com/users/mldxo");
+                const userData = await userResponse.json();
+                const year = new Date(userData.created_at).getFullYear();
                 setCreatedYear(year);
 
                 const img = new Image();
-                img.src = data.avatar_url;
+                img.src = userData.avatar_url;
                 img.onload = () => {
-                    setAvatarUrl(data.avatar_url);
+                    setAvatarUrl(userData.avatar_url);
                     setIsAvatarLoaded(true);
                 };
+
+                const deployResponse = await fetch("https://api.github.com/repos/mldxo/blog-app/deployments");
+                const deployData = await deployResponse.json();
+                if (deployData.length > 0) {
+                    const locale = i18n.language === "en" ? "en-GB" : "pl-PL";
+                    const lastDeployDate = new Date(deployData[0].created_at).toLocaleDateString(locale, {
+                        day: '2-digit',
+                        month: 'long',
+                        year: 'numeric'
+                    });
+                    setLastPageDeploy(lastDeployDate);
+                }
             } catch (error) {
                 console.error("Error fetching data:", error);
             }
         };
-
         fetchData();
-    }, []);
+    }, [t, i18n.language]);
 
     return (
         <div
             className="flex flex-col-reverse h-0 text-center w-full bg-transparent transition-all opacity-50 dark:text-slate-200">
             <div className="flex flex-row mx-auto pb-4 cursor-pointer select-none">
                 <div
-                    className="hover:opacity-70 transition-all duration-300"
+                    className="hover:opacity-70 transition-all duration-300 hidden sm:block"
                     onClick={() => window.location.href = "https://mldxo.github.io"}>
                     {github}
                 </div>
@@ -47,7 +59,7 @@ const Footer = () => {
                     onMouseLeave={() => setIsHovered(false)}
                     onClick={() => window.open("https://github.com/mldxo", "_blank")}
                 >
-                    <div className="flex flex-row justify-center items-center min-w-5 sm:min-w-6 ml-4 sm:ml-2 md:ml-0 -mr-3 sm:-mr-0">
+                    <div className="flex-row justify-center items-center min-w-5 sm:min-w-6 ml-4 sm:ml-2 md:ml-0 -mr-3 sm:-mr-0 hidden sm:flex">
                         <div className="relative">
                             <span className={`transition-opacity duration-300 ${isHovered && isAvatarLoaded ? 'opacity-0' : 'opacity-100'}`}>
                                 &copy;
@@ -60,9 +72,22 @@ const Footer = () => {
                             )}
                         </div>
                     </div>
-                    <div className="ml-3 sm:ml-0">
-                        {createdYear}-{new Date().getFullYear()}
-                        &nbsp; Miłosz Maculewicz - {t("footer.all_rights_reserved")}
+                    <div className="flex ml-3 sm:ml-0">
+                        <p className="flex-col sm:hidden mr-1 sm:mr-0">
+                            &copy;
+                        </p>
+                        <p>
+                            {createdYear}-{new Date().getFullYear()} Miłosz Maculewicz - {t("footer.all_rights_reserved")}
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div
+                className="flex flex-col-reverse h-0 text-center w-full bg-transparent transition-all opacity-50 dark:text-slate-200">
+                <div className="flex flex-row mx-auto pb-4 cursor-normal select-none">
+                    <div
+                        className="hover:opacity-70 transition-all duration-300">
+                        {lastPageDeploy && <p>{t("footer.last_deploy")} {lastPageDeploy}</p>}
                     </div>
                 </div>
             </div>
