16a4ad03b83a8ebab6e6d543513e9706d9fcd9d2
mldxo <miloszmaculewicz@gmail.com>
2024-09-17 16:58:11 +0200

    Add eslint comment

--- frontend/src/functions/themeToggleButton.jsx
+++ frontend/src/functions/themeToggleButton.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from "react";
 import useCheckDarkMode from "../handlers/useCheckDarkmode";
 import { moon, sun } from "../icons";
