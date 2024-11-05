7fbdfca422157f35761b7b13e4b11213f4b29748
milosz275 <miloszmaculewicz@gmail.com>
2024-11-05 12:24:38 +0100

    Update urls

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -20,10 +20,12 @@ urlpatterns = [
     path("list/", BlogPostListView.as_view()),
     path("detail/<pk>/", BlogPostDetailView.as_view()),
     path("delete/<pk>/", BlogPostDeleteView.as_view()),
-    path("like/<pk>/", BlogPostLikeView.as_view()),
-    path("unlike/<pk>/", BlogPostUnlikeView.as_view()),
-    path("comment/<pk>/", BlogPostCommentView.as_view()),
-    path("comment/delete/<pk>/<comment_id>/", BlogPostCommentDeleteView.as_view()),
+    path("like/<int:pk>/", BlogPostLikeView.as_view()),
+    path("unlike/<int:pk>/", BlogPostUnlikeView.as_view()),
+    path("comment/<int:pk>/", BlogPostCommentView.as_view()),
+    path(
+        "comment/delete/<int:pk>/<int:comment_id>/", BlogPostCommentDeleteView.as_view()
+    ),
     path("recent/", RecentPostsView.as_view()),
     path("trending/", TrendingPostsView.as_view()),
     path("popular/", PopularPostsView.as_view()),
